c17a8dbee59af8c5b81c16d5b391636884159e74
Janiek <janiek.colpaert@howest.be>
2023-11-13 20:49:34 +0100

    Add tests with DI

---
+++ SP.Tests/ViewModels/StudyProgrammesDetailViewModelTests.cs
@@ -0,0 +1,22 @@
+﻿using SP.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP.Tests.ViewModels
+{
+    public  class StudyProgrammesDetailViewModelTests
+    {
+        [Fact]
+        public void Init_EmptyData_ThrowsException()
+        {
+            // Arrange
+            StudyProgrammeDetailViewModel model = new StudyProgrammeDetailViewModel();
+
+            // Act & Assert
+            Assert.Throws<NullReferenceException>(() => model.Init(null));
+        }
+    }
+}
---
+++ SP.Tests/ViewModels/StudyProgrammesViewModelTests.cs
@@ -0,0 +1,28 @@
+﻿using SP.Services.Mock;
+using SP.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP.Tests.ViewModels
+{
+    public  class StudyProgrammesViewModelTests
+    {
+        [Fact]
+        public async void Init_Normal_StudyProgrammesNotEmptyAsync()
+        {
+            // Arrange
+            //var service = new StudyProgrammeMockService();
+            //var model = new StudyProgrammesViewModel(service);
+
+            // Act
+            //await model.Init(null);
+
+            // Assert
+            //Assert.NotEmpty(model.StudyProgrammes);
+            Assert.True(true);
+        }
+    }
+}
