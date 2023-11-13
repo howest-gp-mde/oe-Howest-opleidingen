8d795beaf9e74cf56c7c9f8500dc068ec46f3c22
Janiek <janiek.colpaert@howest.be>
2023-11-13 19:14:35 +0100

    Add Unit testing

---
+++ SP.Tests/Converters/LocationToIconConverterTests.cs
@@ -0,0 +1,27 @@
+﻿using SP.Converters;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP.Tests.Converters
+{
+    public class LocationToIconConverterTests
+    {
+
+        [Fact]
+        public void Convert_Valid_Location_ToUrl()
+        {
+            // Arrange
+            LocationToIconConverter converter = new LocationToIconConverter();
+
+            // Act
+            string url = (string)converter.Convert("Kortijk", null, null, null);
+
+            // Assert
+            Assert.Equal("http", url.ToLower().Substring(0, 4));
+        }
+    }
+}
---
+++ SP.Tests/Validators/SettingsValidatorTests.cs
@@ -0,0 +1,59 @@
+﻿using SP.Domain.Models;
+using SP.Domain.Validators;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP.Tests.Validators
+{
+    public class SettingsValidatorTests
+    {
+        [Fact]
+        public void Validate_CorrectInput_Returns_True()
+        {
+            // Arrange
+            SettingsValidator validator = new SettingsValidator();
+            
+            Settings settings = new Settings
+            {
+                Email = "correct@example.com",
+                NumberOfCertificates = 2,
+                ReceiveWeeklyStats = true,
+                UserName = "username"
+            };
+
+            // Act
+            var result = validator.Validate(settings);
+
+            // Assert
+            Assert.True(result.IsValid);
+
+        }
+
+        [Fact]
+        public void Validate_NoUserName_Give_ErrorMessage()
+        {
+            // Arrange
+            var validator = new SettingsValidator();
+            
+            Settings settings = new Settings
+            {
+                Email = "correct@example.com",
+                NumberOfCertificates = 2,
+                ReceiveWeeklyStats = true,
+                UserName = ""
+            };
+
+            // Act
+            var result = validator.Validate(settings);
+            
+            // Assert
+            Assert.Equal("Geef een username op !", result.Errors[0]?.ErrorMessage);
+        }
+
+
+    }
+}
